b3a7d5ee4fe33e1ef8b5150f7904b51a9a8f6d1d
Marko Mijovic <mijovicmarko11@gmail.com>
2022-11-12 17:50:36 -0700

    DB schema

---
+++ Code/src/sql/InitializeDB.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS matchmaking;
+CREATE DATABASE matchmaking;
+USE matchmaking;
+
+DROP TABLE IF EXISTS profile;
+CREATE TABLE user (
+username varchar(20) not null,
+password varchar(20) not null,
+firstName varchar(20) not null,
+lastName varchar(20) not null,
+hourlyRate double,
+isVerified bool not null,
+subscriptionPlan varchar(20),
+paymentEmail varchar(100),
+portfolioLink varchar(100),
+primary key(username)
+);
+
+DROP TABLE IF EXISTS offer;
+CREATE TABLE offer (
+offerId integer not null auto_increment,
+offeredRate double not null,
+offerStatus varchar(20) not null,
+providerId varchar(20) not null,
+clientId varchar(20) not null,
+primary key(offerId),
+foreign key(providerId) references user(username),
+foreign key(clientId) references user(username)
+);
+
+DROP TABLE IF EXISTS contract;
+CREATE TABLE contract (
+contractId integer not null auto_increment,
+offerId integer not null,
+finalStatus varchar(20) not null,
+agreedRate double not null,
+primary key(contractId),
+foreign key(offerId) references offer(offerId)
+);
+
+DROP TABLE IF EXISTS project;
+CREATE TABLE project (
+projectId integer not null auto_increment,
+contractId integer,
+deadline varchar(50),
+progressPercentage double not null,
+projectName varchar(30) not null,
+projectDescription varchar(300),
+projectStatus varchar(30),
+primary key(projectId),
+foreign key(contractId) references contract(contractId)
+);
+
+
