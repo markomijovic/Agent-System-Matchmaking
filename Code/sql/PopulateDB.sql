e16002a7ba13e286f74854defc87ba4643dbe946
Marko Mijovic <mijovicmarko11@gmail.com>
2022-11-15 12:51:54 -0700

    Populate mock db

--- Code/sql/PopulateDB.sql
+++ Code/sql/PopulateDB.sql
@@ -2,4 +2,13 @@ USE matchmaking;
 
 INSERT INTO user
 VALUES ("ProviderAdmin", "pass123", "ProviderName", "ProviderLastName", 50.0, true, "None", "provider@gmail.com", "provider.com"),
-	("ClientAdmin", "pass123", "ClientName", "ClientLastName", 50.0, true, "", "Client@gmail.com", "");
+	("ClientAdmin", "pass123", "ClientName", "ClientLastName", null, true, null, "Client@gmail.com", null);
+    
+INSERT INTO offer
+VALUES (1, 50.0, "Agreed", "ProviderAdmin", "ClientAdmin");
+
+INSERT into contract
+VALUES (1, 1, "Signed", 50.0);
+
+INSERT into project
+VALUES (1, 1, "Dec 30, 2022", 1.0, "TestProject", "Test Description", "In Progress");
