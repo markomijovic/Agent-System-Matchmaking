2734bad620fa7cb130fbf3f26337d16459d05f4e
Marko Mijovic <mijovicmarko11@gmail.com>
2022-11-24 21:26:08 -0700

    Added base

--- Code/src/main/java/Agents/ProjectAgent.java
+++ Code/src/main/java/Agents/ProjectAgent.java
@@ -2,16 +2,65 @@ package main.java.Agents;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import main.java.DB.DBLoader;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class ProjectAgent extends Agent {
-    String nickname = "Project";
-    AID id;
+    protected final DBLoader db = DBLoader.getInstance();
+    protected final String SERVICE_NAME = "project-agent-service";
     protected void setup() {
-        id = new AID(nickname, AID.ISLOCALNAME);
-        System.out.println("Project agent init " + getAID().getName());
+        System.out.println("User agent " + getAID().getName() + " is ready.");
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(SERVICE_NAME);
+        sd.setName(getLocalName());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
     }
 
+    protected Set<AID> searchForService() {
+        Set<AID> agents = new HashSet<>();
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(SERVICE_NAME);
+        dfd.addServices(sd);
+        try {
+            DFAgentDescription[] services = DFService.search(this, dfd);
+            for (DFAgentDescription service : services) {
+                agents.add(service.getName());
+            }
+            return agents;
+        } catch(Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    protected void sendMessage(String text, String id, int type, Set<AID> agents) {
+        ACLMessage message = new ACLMessage(type);
+        message.setContent(text);
+        message.setConversationId(id);
+        for (AID agent : agents) {
+            message.addReceiver(agent);
+        }
+        send(message);
+    }
     protected void takeDown() {
-        System.out.println("Project agent shutdown");
+        try {
+            DFService.deregister(this);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- Code/src/main/java/Agents/UserAgent.java
+++ Code/src/main/java/Agents/UserAgent.java
@@ -1,9 +1,66 @@
 package main.java.Agents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import main.java.DB.DBLoader;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class UserAgent extends Agent {
+    protected final DBLoader db = DBLoader.getInstance();
+    protected final String SERVICE_NAME = "user-agent-service";
     protected void setup() {
-        System.out.println("User agent init " + getAID().getName());
+        System.out.println("User agent " + getAID().getName() + " is ready.");
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(SERVICE_NAME);
+        sd.setName(getLocalName());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    protected Set<AID> searchForService() {
+        Set<AID> agents = new HashSet<>();
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(SERVICE_NAME);
+        dfd.addServices(sd);
+        try {
+            DFAgentDescription[] services = DFService.search(this, dfd);
+            for (DFAgentDescription service : services) {
+                agents.add(service.getName());
+            }
+            return agents;
+        } catch(Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    protected void sendMessage(String text, String id, int type, Set<AID> agents) {
+        ACLMessage message = new ACLMessage(type);
+        message.setContent(text);
+        message.setConversationId(id);
+        for (AID agent : agents) {
+            message.addReceiver(agent);
+        }
+        send(message);
+    }
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
