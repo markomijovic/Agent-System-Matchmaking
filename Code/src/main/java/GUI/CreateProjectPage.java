7e2ce4816612d1fc1c1bfa26a7f65da652212fbe
Marko Mijovic <mijovicmarko11@gmail.com>
2022-12-02 02:40:23 -0700

    Placeholder

---
+++ Code/src/main/java/GUI/CreateProjectPage.java
@@ -0,0 +1,8 @@
+package main.java.GUI;
+
+import main.java.Agents.OrganizerAgent;
+
+public class CreateProjectPage {
+    public CreateProjectPage(OrganizerAgent organizerAgent, String providerUsername) {
+    }
+}
